37a4b08355a87b2a59665192c7193fefcd5601e8
kelvin389 <kelvinl.leung@gmail.com>
2023-12-17 21:40:48 -0800

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -8,8 +8,8 @@ data_to_get = [
     ("banjo", "https://www.songfacts.com/category/songs-featuring-a-banjo"),
 ]
 
-# tuple(str: data_name, list: list of data entires)
-data = []
+# key: data_name, data: list of data entries
+data = {}
 
 driver = webdriver.Firefox()
 for data_name, data_link in data_to_get:
@@ -18,15 +18,20 @@ for data_name, data_link in data_to_get:
     driver.get(data_link)
     elements = driver.find_elements(By.XPATH, "//ul[contains(@class, 'browse-list-purple space-bot')]/li")
     if not elements:
-        print("captcha detected. exiting")
+        print("captcha detected. exiting. visit songfacts.com on your own browser and complete the captcha before running again.")
         driver.close()
         exit(1)
     for e in elements:
         if e.text:
             this_data.append(e.text)
 
-    data.append((data_name, this_data))
+    data[data_name] = this_data
 
 driver.close()
 
-print(data)
+for d in data_to_get:
+    name = d[0]
+    print(name)
+    
+    for song in data[name]:
+        print("\t", song)
